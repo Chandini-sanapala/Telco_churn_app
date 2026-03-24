bcfc64f5aadc2275e7fc6029952c6cad2b3b9445
Chandini-sanapala <sanapalachandini256@gmail.com>
2026-03-24 22:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,36 @@ if predict_clicked:
         st.error("⚠️ Customer will CHURN")
     else:
         st.success("✅ Customer will STAY")
+        
+if prediction == 1:
+    st.error("⚠️ Customer is likely to CHURN")
 
+    st.markdown("### 💡 Suggestions to Retain Customer:")
+
+    if tenure < 12:
+        st.write("👉 Offer loyalty discounts for new customers")
+
+    if Contract == "Month-to-month":
+        st.write("👉 Encourage long-term contract plans (1 year / 2 year)")
+
+    if TechSupport == "No":
+        st.write("👉 Provide free or discounted tech support")
+
+    if OnlineSecurity == "No":
+        st.write("👉 Add security features to increase trust")
+
+    if PaymentMethod == "Electronic check":
+        st.write("👉 Suggest automatic payment methods (credit card / bank transfer)")
+
+    if MonthlyCharges > 70:
+        st.write("👉 Provide discounts or better pricing plans")
+
+else:
+    st.success("✅ Customer is likely to STAY")
+
+    st.markdown("### 🎉 Positive Insights:")
+    st.write("👉 Customer is satisfied with services")
+    st.write("👉 Low risk of churn")
 # ==========================================================
 # NAVIGATION
 # ==========================================================
